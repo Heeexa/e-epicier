c418bf4af44c8aa07a3aee8f13996fd965816160
Heeexa <61749727+Heeexa@users.noreply.github.com>
2023-02-25 23:10:40 +0100

    Update settings.py

--- e_epicier/settings.py
+++ e_epicier/settings.py
@@ -140,7 +140,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 CSRF_TRUSTED_ORIGINS=['https://e-epicier-production.up.railway.app/']
