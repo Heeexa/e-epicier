e421d2dd7ac293159d33776af443131a5e3417d6
Heeexa <61749727+Heeexa@users.noreply.github.com>
2023-02-25 23:40:15 +0100

    Update settings.py

--- e_epicier/settings.py
+++ e_epicier/settings.py
@@ -138,7 +138,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-CSRF_COOKIE_SECURE = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
